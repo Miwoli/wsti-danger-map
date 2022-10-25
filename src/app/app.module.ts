8ff33664fb587abb7c2e5e6434f9f442ff132e2b
Michał Woliński <1531829-Miwoli@users.noreply.gitlab.com>
2022-10-25 19:46:18 +0200

    layout + autodeploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,6 +20,8 @@ import { TopNavComponent } from './top-nav/top-nav.component';
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
+    MatToolbarModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
